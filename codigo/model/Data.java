0dc66bf0c647b30a6b4cfff3cf9a331589d6ff39
Maria Clara <mariaclariagomes@gmail.com>
2024-10-15 11:04:05 -0300

    Update Data

--- codigo/model/Data.java
+++ codigo/model/Data.java
@@ -1,5 +1,19 @@
 package codigo.model;
 
 public class Data {
-    
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Data (int dia, int mes, int ano){
+        this.ano = ano;
+        this.dia = dia;
+        this.mes = mes;
+
+    }
+    @Override
+
+    public String toString(){
+        return String.format("%02d/%02d/%04d", dia, mes, ano);
+    }
 }
